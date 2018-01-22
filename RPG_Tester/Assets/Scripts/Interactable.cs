17f265e4a5e911dd38db9fd47dcdf1340f663ef1
Joe Pignataro <joe.pignataro16@gmail.com>
2018-01-23 00:05:17 +1100

    23/01/2018
    Added dialogue system as well as preliminary item iteractions and object interactions

---
+++ RPG_Tester/Assets/Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent playerAgent;
+    private bool hasInteracted;
+
+    /// <summary>
+    /// Take Player to Interactable Object, Before Interacting.
+    /// </summary>
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent)
+    {
+        hasInteracted = false;
+        this.playerAgent = playerAgent;
+
+        playerAgent.stoppingDistance = 2.5f;
+        playerAgent.destination = this.transform.position;
+
+
+        Interact();
+       
+    }
+
+
+    private void Update()
+    {
+        if(playerAgent != null && !playerAgent.pathPending)
+        {
+            if(!hasInteracted && playerAgent.remainingDistance <= playerAgent.stoppingDistance)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Base Interaction Function.
+    /// </summary>
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with Base Class");
+    }
+
+}
